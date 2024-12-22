c354684a0c599bb3e488705fcc5ed6d6f062817f
ShashidharNL <Shashidharln18@gmail.com>
2024-12-22 19:56:02 +0530

    Initial commit

---
+++ src/data/WorkData.js
@@ -0,0 +1,63 @@
+// work data
+
+export const Work = [
+  {
+    id: 1,
+    name: "Gemini Clone",
+    description:
+      "A dynamic web app built with Vite and React, leveraging Gemini's API for interactive features and styled-components for modern design.",
+    tags: ["react", "vite", "css", "styledComponents", "APIIntegration"],
+    demo: "https://gemini-clone-seven-phi.vercel.app/",
+    github: "https://github.com/ShashidharNL/GeminiClone",
+  },
+
+  {
+    id: 2,
+    name: "Custom Code Editor",
+    description:
+      "A powerful ReactJS-based code editor with real-time execution for JavaScript and Python, theme toggling, file uploads, and an integrated terminal. Built using Monaco Editor and Node.js for seamless functionality.",
+    tags: ["react", "monacoEditor", "nodejs", "codeExecution", "javascript"],
+
+    demo: "https://my-code-editor-inky.vercel.app/",
+    github: "https://github.com/ShashidharNL/MyCodeEditor",
+  },
+  {
+    id: 3,
+    name: "Ants Analyzer",
+    description:
+      "A ReactJS and CSS-based stock analysis tool for mid-cap, small-cap, large-cap stocks, delivering insights on the best returns.",
+    tags: ["react", "css", "stockAnalysis", "apiIntegration"],
+
+    demo: "https://antsanalyzer.com/",
+    github: "#",
+  },
+  // {
+  //     id:4,
+  //     name:"Sidebar Menu",
+  //     description:"A cool looking sidebar menu build using ReactJS and styledComponents.It has really smooth animations.",
+  //     tags:["react","styledComponents","react-router"],
+
+  //     demo:"https://react-sidebar.vercel.app/",
+  //     github:"https://github.com/codebucks27/react-sidebar"
+
+  // },{
+  //     id:5,
+  //     name:"Todo App(React + Redux)",
+  //     description:"A todo app build using Redux-toolkit and framer-motion for smooth animations.",
+  //     tags:["react","redux","framer-motion"],
+
+  //     demo:"http://react-redux-todo-app-git-main-codebucks27.vercel.app/",
+  //     github:"https://github.com/codebucks27/React-Redux-Todo-App"
+
+  // },
+  // {
+  //     id:6,
+  //     name:"Responsive Navigation Menu",
+  //     description:"A reponsive navbar menu component in ReactJS with react-router functionalities. Built from scratch.",
+  //     tags:["react","react-router","css"],
+
+  //     demo:"http://react-responsive-navbar-codebucks27.vercel.app/",
+  //     github:"https://github.com/codebucks27/React-responsive-navbar"
+
+  // },
+];
